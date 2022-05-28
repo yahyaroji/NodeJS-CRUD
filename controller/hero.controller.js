f3d18b65154c2d1e91195617d279bd8078790fd9
yahyaroji <yahyaroji9@gmail.com>
2022-05-28 20:59:59 +0700

    perbaikan tombol edit

--- controller/hero.controller.js
+++ controller/hero.controller.js
@@ -17,7 +17,7 @@ router.get("/:id", (request, response) => {
 router.post("/update", (request, response) => {
   const data = {
     id: request.body.id,
-    name: request.body.hero,
+    //name: request.body.name,
     role: request.body.role,
     skill: request.body.skill,
   };
